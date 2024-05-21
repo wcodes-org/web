8dd41995545a09f6e1fce6a0fd6d8e1448616963
Ujjwal Singh <ujjwalsingh@outlook.com>
2024-05-22 04:54:24 +0530

    add render.sh for build

---
+++ render.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+/app/Tiggu/build.sh /app/Site/Project
+
+shopt -s extglob
+
+copy_dot_dirs() {
+  local source_dir=$1
+  local dest_dir=$2
+  
+  shopt -s extglob
+
+  cp -r $source_dir/* $dest_dir
+  for dir in "$source_dir"/.!(.|git); do
+    if [ -d "$dir" ] || [ -f "$dir" ]; then
+      cp -r "$dir" "$dest_dir"
+    fi
+  done
+}
+
+# moving in app files after web files inorder to overwrite
+
+copy_dot_dirs /app/Site/Project/interim ./interim
+copy_dot_dirs /app/Site/Project/public ./public
